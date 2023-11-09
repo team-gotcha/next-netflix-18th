318f8d9b7b21041e08a2be84ea8af52a4b7c3875
mod_siw <pvq_71@ewhain.net>
2023-11-10 02:30:14 +0900

    style : MainControlBox style

--- app/components/ItemList.tsx
+++
@@ -1,8 +0,0 @@
-import React from "react";
-import styled from "styled-components";
-import Image from "next/image";
-
-const ItemList = () => {
-  return <></>;
-};
-export default ItemList;
--- app/main/page.tsx
+++ app/main/page.tsx
@@ -2,11 +2,13 @@
 import styled from "styled-components";
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
+import MainControlBox from "../components/MainControlBox";
 
 const MainPage = () => {
   return (
     <Wrapper>
       <Header />
+      <MainControlBox />
       <NavBar />
     </Wrapper>
   );
