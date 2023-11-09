565fe638129eec0a72ba5ccb1573fffd55973f9e
silver <ekdldkaa@naver.com>
2023-11-10 01:06:12 +0900

    [feat] axios 인스턴스화, data fetching test

---
+++ api/movieApi.ts
@@ -0,0 +1,21 @@
+import api from './index';
+
+const API_KEY = '6d482b614139b65c7359f30521c4deb2';
+
+export const getNowPlaying = () => {
+  return api
+    .get(`movie/now_playing?api_key=${API_KEY}`)
+    .then((res) => res.data);
+};
+
+export const getTopRated = () => {
+  return api.get(`movie/top_rated?api_key=${API_KEY}`).then((res) => res.data);
+};
+
+export const getPopular = () => {
+  return api.get(`movie/popular?api_key=${API_KEY}`).then((res) => res.data);
+};
+
+export const getUpcoming = () => {
+  return api.get(`movie/upcoming?api_key=${API_KEY}`).then((res) => res.data);
+};
--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,33 @@
+'use client';
+import {
+  getTopRated,
+  getNowPlaying,
+  getPopular,
+  getUpcoming,
+} from '@/api/movieApi';
+import React, { useEffect, useState } from 'react';
+
 export default function Home() {
+  //데이터 받아오기
+  const fetchData = async () => {
+    const nowPlayingData = await getNowPlaying();
+    const topRatedData = await getTopRated();
+    const popularData = await getPopular();
+    const upcomingData = await getUpcoming();
+
+    // console.log('Now Playing:', nowPlayingData);
+    // console.log('Top Rated:', topRatedData);
+    console.log('Popular:', popularData.results[0].title);
+    // console.log('Upcoming:', upcomingData);
+  };
+
+  useEffect(() => {
+    fetchData();
+  }, []);
+
   return (
     <div>
-      <h1>ThisHome/</h1>
+      <h1>ThisHome</h1>
     </div>
   );
 }
