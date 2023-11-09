4ffd269735cff4416f8fe559c230b989c9ed9a24
mod_siw <pvq_71@ewhain.net>
2023-11-10 03:56:57 +0900

    style : MainImage style

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -1,12 +1,12 @@
-'use client';
+"use client";
 import {
   getTopRated,
   getNowPlaying,
   getPopular,
   getUpcoming,
   getImageUrl,
-} from '../../api/movieApi';
-import React, { useEffect, useState } from 'react';
+} from "../../api/movieApi";
+import React, { useEffect, useState } from "react";
 import styled from "styled-components";
 import Header from "../components/Header";
 import NavBar from "../components/NavBar";
@@ -39,23 +39,9 @@ export default function Main() {
   return (
     <Wrapper>
       <Header />
-            <ul>
-        {popularData ? (
-          popularData.results.map((movie) => (
-            <li key={movie.id}>
-              <img src={getImageUrl(movie.poster_path)} alt="movie poster" />
-              <p>{movie.title}</p>
-              <p>{movie.vote_average}</p>
-            </li>
-          ))
-        ) : (
-          <h1>Loading...</h1>
-        )}
-      </ul>
 
       <MainControlBox />
       <NavBar />
-
     </Wrapper>
   );
 }
