18b555c211a1740beccc2f54968d628e5b4b1b66
silver <ekdldkaa@naver.com>
2023-11-15 23:05:59 +0900

    [feat] API연동 CSR방식으로 변경

--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -3,22 +3,24 @@ import { usePathname, useRouter } from 'next/navigation';
 import { getMovies } from '@/api/movieApi';
 import { useEffect, useState } from 'react';
 
-export const getData = async (id: string) => {
-  const movieData = await getMovies(`movie/${id}`);
-
-  return movieData;
-};
-
-export default async function Detail() {
+export default function Detail() {
   let movieId = '0';
   //pathvariable 추출
   const router = usePathname();
   const numberPart = router.match(/\d+/);
+  const [data, setData] = useState();
   if (numberPart) {
     movieId = numberPart[0];
-    const data = await getData(movieId);
-    console.log(data.title);
   }
+  useEffect(() => {
+    const fetchData = async (id: string) => {
+      setData(await getMovies(`movie/${id}`));
+    };
+
+    fetchData(movieId);
+    console.log(data);
+  }, [numberPart]);
+
   return (
     <div>
       <h1>{movieId}상세페이지</h1>
