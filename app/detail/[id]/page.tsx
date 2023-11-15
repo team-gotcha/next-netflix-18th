2fd517b0c0a5201b8f19ae46449f5366886c8406
silver <ekdldkaa@naver.com>
2023-11-15 22:37:51 +0900

    [feat] api 통신

--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -1,16 +1,24 @@
 'use client';
 import { usePathname, useRouter } from 'next/navigation';
+import { getMovies } from '@/api/movieApi';
 import { useEffect, useState } from 'react';
 
-export default function Detail() {
+export const getData = async (id: string) => {
+  const movieData = await getMovies(`movie/${id}`);
+
+  return movieData;
+};
+
+export default async function Detail() {
   let movieId = '0';
   //pathvariable 추출
   const router = usePathname();
   const numberPart = router.match(/\d+/);
   if (numberPart) {
     movieId = numberPart[0];
+    const data = await getData(movieId);
+    console.log(data.title);
   }
-
   return (
     <div>
       <h1>{movieId}상세페이지</h1>
