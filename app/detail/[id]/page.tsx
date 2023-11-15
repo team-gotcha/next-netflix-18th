c2407d978cd92e7ccbcd83bb02fc485af2f24f6c
silver <ekdldkaa@naver.com>
2023-11-15 23:58:07 +0900

    [feat] API연동, 스켈레톤 구성

--- app/detail/[id]/page.tsx
+++ app/detail/[id]/page.tsx
@@ -1,14 +1,24 @@
 'use client';
 import { usePathname, useRouter } from 'next/navigation';
-import { getMovies } from '@/api/movieApi';
+import { getImageUrl, getMovies } from '@/api/movieApi';
 import { useEffect, useState } from 'react';
+import styled from 'styled-components';
+import Image from 'next/image';
+
+interface dataProps {
+  poster_path: string;
+  title: string;
+  overview: string;
+}
 
 export default function Detail() {
   let movieId = '0';
   //pathvariable 추출
   const router = usePathname();
   const numberPart = router.match(/\d+/);
-  const [data, setData] = useState();
+
+  const [data, setData] = useState<dataProps>();
+
   if (numberPart) {
     movieId = numberPart[0];
   }
@@ -22,8 +32,26 @@ export default function Detail() {
   }, [numberPart]);
 
   return (
-    <div>
-      <h1>{movieId}상세페이지</h1>
-    </div>
+    <Wrapper>
+      {data && (
+        <Image
+          src={getImageUrl(data.poster_path)}
+          alt="movie poster"
+          width={100}
+          height={100}
+        />
+      )}
+      <PlayBut />
+      <Text>
+        <Title></Title>
+        <DescribeText></DescribeText>
+      </Text>
+    </Wrapper>
   );
 }
+
+const Wrapper = styled.div``;
+const PlayBut = styled.div``;
+const Text = styled.div``;
+const Title = styled.div``;
+const DescribeText = styled.div``;
